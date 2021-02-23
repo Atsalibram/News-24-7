de3b0f0ecd979e589ff12a119582b91a62c09de5
atsalibram <atsalibaram@gmail.com>
2021-02-23 22:46:51 +0300

    changes to config

--- app/views.py
+++ app/views.py
@@ -9,6 +9,7 @@ def index():
     View root page function that returns the index page and its data
     '''
     message = 'Hello World'
+    title = 'Home - Welcome to the best News Review Website online'
     return render_template('index.html',message = message)
 
 # views
@@ -18,4 +19,5 @@ def movie(news_id):
     '''
     View movie page function that returns the news details page and its data
     '''
+    message = 'Hello World'
     return render_template('news.html',id = news_id)
