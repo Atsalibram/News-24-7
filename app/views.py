489d4f663fce107f1b8a2a1b76913575ae49e1c8
atsalibram <atsalibaram@gmail.com>
2021-02-23 22:23:18 +0300

    added code to news file

--- app/views.py
+++ app/views.py
@@ -12,10 +12,10 @@ def index():
     return render_template('index.html',message = message)
 
 # views
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+@app.route('/news/<int:news_id>')
+def movie(news_id):
 
     '''
-    View movie page function that returns the movie details page and its data
+    View movie page function that returns the news details page and its data
     '''
-    return render_template('movie.html',id = movie_id)
+    return render_template('news.html',id = news_id)
